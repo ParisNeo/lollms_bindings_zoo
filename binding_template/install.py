5b35eb0641862d7bfd12ac0355bf3bc9491b7104
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-06-16 21:10:36 +0200

    bugfix

--- binding_template/install.py
+++ binding_template/install.py
@@ -7,7 +7,7 @@ from lollms.helpers import ASCIIColors
 class Install(BindingInstaller):
     def __init__(self, config:LOLLMSConfig=None, force_reinstall=False):
         # Build parent
-        super().__init__(config, False)
+        super().__init__(config)
         # Get the current directory
         current_dir = Path(__file__).resolve().parent
         install_file = current_dir / ".installed"
