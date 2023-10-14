0942483eb1434ec766cfe7498bcf63c044212511
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-10-14 10:39:02 +0200

    upgraded comments

--- exllama2/__init__.py
+++ exllama2/__init__.py
@@ -2,6 +2,8 @@
 # Project       : lollms
 # File          : binding.py
 # Author        : ParisNeo with the help from bartowski
+# Underlying 
+# engine author : turboderp 
 # license       : Apache 2.0
 # Description   : 
 # This is an interface class for lollms bindings.
