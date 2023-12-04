91841b2aa72aff3c04d215acd4794ec3872c1410
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-12-04 23:48:20 +0100

    added scraper for future

--- open_router/models_scraper.py
+++ open_router/models_scraper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import json
+import yaml
 
 # URL of the page to scrape
 url = 'https://openrouter.ai/docs#quick-start'
@@ -13,19 +13,22 @@ if response.status_code == 200:
     # Parse the HTML content of the page
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # Assuming the model names and IDs are in a table with class 'model-table'
-    # You will need to inspect the HTML and adjust the class name accordingly
+    # Find the table with the class 'table-fixed w-full'
     models_table = soup.find('table', {'class': 'table-fixed w-full'})
     
-    # Assuming each model is in a row with class 'model-row'
-    # and the model name and ID are in separate cells
-    # You will need to inspect the HTML and adjust the class names accordingly
-    model_rows = models_table.find_all('tr', {'class': 'model-row'})
+    # Assuming each model is in a row within the table body (tbody)
+    model_rows = models_table.find('tbody').find_all('tr')
     
     models_data = []
     for row in model_rows:
-        model_name = row.find('td', {'class': 'model-name'}).text.strip()
-        model_id = row.find('td', {'class': 'model-id'}).text.strip()
+        # Find the anchor and code tags within the row
+        model_name_tag = row.find('a')
+        model_id_tag = row.find('code')
+        
+        # Extract the text from the tags
+        model_name = model_name_tag.text.strip() if model_name_tag else ""
+        model_id = model_id_tag.text.strip() if model_id_tag else ""
+        model_href = model_name_tag['href'] if model_name_tag else ""
         
         model_info = {
             "category": "generic",
@@ -34,7 +37,7 @@ if response.status_code == 200:
             "last_commit_time": "",  # Placeholder for last commit time
             "license": "commercial",
             "model_creator": "",  # Placeholder for model creator
-            "model_creator_link": "",  # Placeholder for model creator link
+            "model_creator_link": model_href,  # Extracted href from the anchor tag
             "name": model_id,
             "quantizer": None,
             "rank": 0.0,  # Placeholder for rank
@@ -48,12 +51,12 @@ if response.status_code == 200:
         }
         models_data.append(model_info)
     
-    # Convert the list of dictionaries to JSON
-    models_json = json.dumps(models_data, indent=2)
+    # Convert the list of dictionaries to YAML
+    models_yaml = yaml.dump(models_data)
     
-    # Output the JSON or write it to a file
-    print(models_json)
-    with open('models.json', 'w') as json_file:
-        json_file.write(models_json)
+    # Output the YAML or write it to a file
+    print(models_yaml)
+    with open('models.yaml', 'w') as yaml_file:
+        yaml_file.write(models_yaml)
 else:
     print(f"Failed to retrieve the webpage, status code: {response.status_code}")
