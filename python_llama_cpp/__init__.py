3fb8c1b4158755334fd39c651eb80bb2b654af56
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-12-22 14:18:22 +0100

    fixed problems

--- python_llama_cpp/__init__.py
+++ python_llama_cpp/__init__.py
@@ -147,14 +147,14 @@ class LLAMA_Python_CPP(LLMBinding):
         
         if current_platform == 'Linux':
             if self.config.enable_gpu:
-                subprocess.run(['pip', 'install', 'llama-cpp-python'], env={'CMAKE_ARGS': "-DLLAMA_CUBLAS=on"})
+                subprocess.run(['pip', 'install', "--upgrade", 'llama-cpp-python'], env={'CMAKE_ARGS': "-DLLAMA_CUBLAS=on"})
             else:
-                subprocess.run(['pip', 'install', 'llama-cpp-python'])
+                subprocess.run(['pip', 'install', "--upgrade", 'llama-cpp-python'])
         elif current_platform == 'Windows':
             if self.config.enable_gpu:
-                subprocess.run(['pip', 'install', 'llama-cpp-python'], env={'CMAKE_ARGS': "-DLLAMA_CUBLAS=on"})
+                subprocess.run(['pip', 'install', "--upgrade", 'llama-cpp-python'], env={'CMAKE_ARGS': "-DLLAMA_CUBLAS=on"})
             else:
-                subprocess.run(['pip', 'install', 'llama-cpp-python'])
+                subprocess.run(['pip', 'install', "--upgrade", 'llama-cpp-python'])
         elif current_platform == 'Darwin':
             gpu_support = 'y' if subprocess.run(['system_profiler', 'SPDisplaysDataType'], capture_output=True).stdout.decode().lower().find('gpu') != -1 else 'n'
             
