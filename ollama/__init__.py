5e2aebceaae4b4022037652dcdfccf62cdb57de8
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-01-09 00:42:03 +0100

    upgraded

--- ollama/__init__.py
+++ ollama/__init__.py
@@ -79,7 +79,7 @@ class Ollama(LLMBinding):
 
         binding_config = TypedConfig(
             ConfigTemplate([
-                {"name":"address","type":"str","value":"http://127.0.0.1:5000","help":"The server address"},
+                {"name":"address","type":"str","value":"http://127.0.0.1:11434","help":"The server address"},
                 {"name":"completion_format","type":"str","value":"instruct","options":["instruct","chat"], "help":"The format supported by the server"},
                 {"name":"ctx_size","type":"int","value":4090, "min":512, "help":"The current context size (it depends on the model you are using). Make sure the context size if correct or you may encounter bad outputs."},
                 {"name":"server_key","type":"str","value":"", "help":"The API key to connect to the server."},
