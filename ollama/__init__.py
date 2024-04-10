113177a924638f5ea9234b10e8aefc6db93e407a
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-04-11 00:40:27 +0200

    upgraded ollama

--- ollama/__init__.py
+++ ollama/__init__.py
@@ -361,6 +361,8 @@ class Ollama(LLMBinding):
         """
         
         return entries
+    
+
 if __name__=="__main__":
     from lollms.paths import LollmsPaths
     from lollms.main_config import LOLLMSConfig
@@ -414,22 +416,6 @@ if __name__=="__main__":
     oai.binding_config.save()
     config.binding_name= "ollama"
 
-    config.model_name="llama2"
+    config.model_name="mistral:latest"
     config.save_config()
 
-
-if __name__=="__main__":
-    from lollms.paths import LollmsPaths
-    from lollms.main_config import LOLLMSConfig
-    from lollms.app import LollmsApplication
-    from pathlib import Path
-    root_path = Path(__file__).parent
-    lollms_paths = LollmsPaths.find_paths(tool_prefix="",force_local=True, custom_default_cfg_path="configs/config.yaml")
-    config = LOLLMSConfig.autoload(lollms_paths)
-    lollms_app = LollmsApplication("",config, lollms_paths, False, False,False, False)
-
-    oai = Ollama(config, lollms_paths,lollmsCom=lollms_app)
-    oai.install()
-    config.binding_name= "ollama"
-    config.model_name="llama2:latest"
-    config.save_config()
