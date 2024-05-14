b6477c46bffb40e8a3689737157cee27b9f5ebe0
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-05-14 10:27:42 +0200

    Update __init__.py

--- open_ai/__init__.py
+++ open_ai/__init__.py
@@ -299,7 +299,7 @@ class OpenAIGPT(LLMBinding):
         """
         if self.binding_config.config.turn_on_cost_estimation:
             self.binding_config.config["total_input_tokens"] +=  len(self.tokenize(prompt))          
-            self.binding_config.config["total_input_cost"] =  self.binding_config.config["total_input_tokens"] * self.input_costs_by_model[self.config["model_name"]] /1000
+            self.binding_config.config["total_input_cost"] =  self.binding_config.config["total_input_tokens"] * (self.input_costs_by_model[self.config["model_name"]] if self.config["model_name"] in self.input_costs_by_model else 0) /1000
         try:
             default_params = {
                 'temperature': 0.7,
@@ -354,7 +354,7 @@ class OpenAIGPT(LLMBinding):
             trace_exception(ex)
         if self.binding_config.config.turn_on_cost_estimation:
             self.binding_config.config["total_output_tokens"] +=  len(self.tokenize(output))          
-            self.binding_config.config["total_output_cost"] =  self.binding_config.config["total_output_tokens"] * self.output_costs_by_model[self.config["model_name"]]/1000    
+            self.binding_config.config["total_output_cost"] =  self.binding_config.config["total_output_tokens"] * self.output_costs_by_model[self.config["model_name"]] if self.config["model_name"] in self.output_costs_by_model else 0/1000    
             self.binding_config.config["total_cost"] = self.binding_config.config["total_input_cost"] + self.binding_config.config["total_output_cost"]
             self.info(f'Consumed {self.binding_config.config["total_output_cost"]}$')
             self.binding_config.save()
